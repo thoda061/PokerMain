5695ba4eb1598cbb466c570e20fbb59b1cf54bb0
thoda061 <thoda061@mds-xa201s-p002.vdi.student.otago.ac.nz>
2017-07-18 10:46:41 +1200

    Commenting code and fixing output.

--- src/Card.java
+++ src/Card.java
@@ -1,121 +1,150 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author dathomson
+/** Cosc 326 Etude 3
+ *	Card.java - class for storing info about an individual
+ * card in a poker hand.
+ * @author Daniel Thomson ID:5040702
  */
 public class Card {
     
-    private String suit;
-    private String value;
-    private int suitVal;
-    private int valueNum;
-    
-    public Card (String card) {
-        suit = card.substring(card.length()-1);
-        suitVal = setSuitVal();
-        value = card.substring(0,card.length()-1);
-        valueNum = setNumVal();
-    }
-    
-    private int setNumVal() {
-        int val = 0;
-        try {
-            val = Integer.valueOf(value);
-            if(val == 1) {
-                val = 14;
-            }
-        } catch (Exception e) {
-            String letterVal = value.toLowerCase();
-            switch (letterVal) {
-                case "a":
-                    val = 14;
-                    break;
-                case "j":
-                    val = 11;
-                    break;
-                case "q":
-                    val = 12;
-                    break;
-                case "k":
-                    val = 13;
-                    break; 
-            }   
-        }
-        return val;
-    }
-    
-    private int setSuitVal() {
-        String suitLetter = suit.toLowerCase();
-        int val = 0;
-        switch (suitLetter) {
-            case "c":
-                val = 1;
-					 break;
-            case "d":
-                val = 2;
-					 break;
-            case "h":
-                val = 3;
-					 break;
-            case "s":
-                val = 4;
-					 break;
-        }
-        return val;
-    }
+	//String represent suit of the card
+   private String suit;
+	//String representing value of the card
+   private String value;
+	// Int representing suit of the card
+   private int suitVal;
+	//Int representing value of the card
+   private int valueNum;
+   
+	/**
+	 * Constructor that takes a String representing a card 
+	 *	(suit and value)
+	 * @param card String representing card. 
+	 */
+   public Card (String card) {
+      suit = card.substring(card.length()-1);
+      suitVal = setSuitVal();
+      value = card.substring(0,card.length()-1);
+      valueNum = setNumVal();
+   }
+   
+	/**
+	 * Return the int representing the value of the card, using the
+	 * the String set in value.
+	 * @return Int representing card value;
+	 */
+   private int setNumVal() {
+      int val = 0;
+      try {
+         val = Integer.valueOf(value);
+			//Gives aces highest value
+         if(val == 1) {
+            val = 14;
+         }
+      } catch (Exception e) {
+			//Deals with letter input for value
+         String letterVal = value.toLowerCase();
+         switch (letterVal) {
+            case "a":
+               val = 14;
+               break;
+            case "j":
+               val = 11;
+               break;
+            case "q":
+               val = 12;
+               break;
+            case "k":
+               val = 13;
+               break; 
+         }   
+      }
+      return val;
+   }
+   /**
+	 * Return the int representing the suit of the card, using the String
+	 * set in suit.
+	 * @return Int representing suit
+	 */
+   private int setSuitVal() {
+      String suitLetter = suit.toLowerCase();
+      int val = 0;
+      switch (suitLetter) {
+         case "c":
+            val = 1;
+				break;
+         case "d":
+            val = 2;
+				break;
+         case "h":
+            val = 3;
+				break;
+         case "s":
+            val = 4;
+				break;
+      }
+      return val;
+   }
+	 
+	//Getter Methods
+   public int getValueNum() {
+      return valueNum;
+   }
 
-    public String getSuit() {
-        return suit;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public int getValueNum() {
-        return valueNum;
-    }
-
-    public int getSuitVal() {
-        return suitVal;
-    }
+   public int getSuitVal() {
+      return suitVal;
+   }
+   
+	//To String Method
+   public String toString() {
+		StringBuilder sb = new StringBuilder();
+		if(valueNum > 10){
+			switch(valueNum) {
+				case 11:
+					sb.append("J");
+					break;
+				case 12:
+					sb.append("Q");
+					break;
+				case 13:
+					sb.append("K");
+					break;
+				case 14:
+					sb.append("A");
+					break;
+			}
+		} else {
+			sb.append(value);
+		}
+		sb.append(suit.toUpperCase());
+      return sb.toString();
+   }
     
-    public String toString() {
-        return value+suit;
-    }
-    
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 23 * hash + this.suitVal;
-        hash = 23 * hash + this.valueNum;
-        return hash;
-    }
+   @Override
+   public int hashCode() {
+      int hash = 3;
+      hash = 23 * hash + this.suitVal;
+      hash = 23 * hash + this.valueNum;
+      return hash;
+   }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Card other = (Card) obj;
-        if (this.suitVal != other.suitVal) {
-            return false;
-        }
-        if (this.valueNum != other.valueNum) {
-            return false;
-        }
-        return true;
-    }
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+         return true;
+      }
+      if (obj == null) {
+         return false;
+      }
+      if (getClass() != obj.getClass()) {
+         return false;
+      }
+      final Card other = (Card) obj;
+      if (this.suitVal != other.suitVal) {
+         return false;
+      }
+      if (this.valueNum != other.valueNum) {
+         return false;
+      }
+      return true;
+   }
     
 }
--- src/PokerMain.java
+++ src/PokerMain.java
@@ -1,107 +1,133 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 
-/**
- *
- * @author thoda061
+/** Cosc 326 Etude 3
+ *	PokerMain.java - Main class
+ * @author Daniel Thomson ID:5040702
  */
 public class PokerMain {
 	
-    private static final String PATTERN ="((([1]?[0-9])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])((([1]?[0-9])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])((([1]?[0-9])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])((([1]?[0-9])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])((([1]?[0-9])|[KQJAkqja])[sdhcSDHC])";
-    private static String sep;
-
-    public static void main(String[]args) {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
-            String input = sc.nextLine();
-            boolean match = Pattern.matches(PATTERN, input);
-            if(match) {
-                ArrayList<Card> sepInput = seperate(input);
-                sepInput = sort(sepInput);
-                if(!(validate(sepInput))) {
-                    System.out.println("Invalid: " + input);
-                }else{
-						combine(sepInput);
-					 }
-            } else {
-                System.out.println("Invalid: " + input);
-            }
-        }
-    }
+	//Regular Expression for detecting correct format of input.
+   private static final String PATTERN ="(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC])";
 	
-    public static ArrayList<Card> seperate(String input) {
-        char seperator;
-        int lastSep = 0,numCards = 0;
-        ArrayList<Card> hand = new ArrayList();
-        if(input.contains("/")) {
-                seperator = '/';
-                sep = "/";
-        } else if (input.contains("-")) {
-                seperator = '-';
-                sep = "-";
-        } else {
-                seperator = ' ';
-                sep = " ";
-        }
-        for(int i = 0; i < input.length(); i++) {
-            if(input.charAt(i) == seperator) {
-                hand.add(new Card(input.substring(lastSep, i)));
-                numCards++;
-                lastSep = i+1;
-                if(numCards == 4) {
-                    hand.add(new Card(input.substring(lastSep)));
-                    numCards++;
-                }
+	/**
+	 * Reads in line, determines whether it match regex, if it does separates cards
+	 * into ArrayList, sort them from lowest to highest, checks for duplicate
+	 * cards, then outputs sorted hand. If input does match regex or has 
+	 * duplicates prints invalid.
+	 * @param args arguments (not used)
+	 */
+   public static void main(String[]args) {
+      Scanner sc = new Scanner(System.in);
+      while(sc.hasNextLine()) {
+         String input = sc.nextLine();
+         boolean match = Pattern.matches(PATTERN, input);
+         if(match) {
+            ArrayList<Card> sepInput = seperate(input);
+            sepInput = sort(sepInput);
+            if(!(validate(sepInput))) {
+               System.out.println("Invalid: " + input);
+            }else{
+					combine(sepInput);
+				}
+         } else {
+            System.out.println("Invalid: " + input);
+         }
+      }
+   }
+	
+	/**
+	 * Takes string as input and separates it in to individual Card objects
+	 * stored in ArrayList.
+	 * @param input String representing hand of cards.
+	 * @return ArrayList of Card objects.
+	 */
+   public static ArrayList<Card> seperate(String input) {
+      char seperator;
+      int lastSep = 0,numCards = 0;
+      ArrayList<Card> hand = new ArrayList();
+      if(input.contains("/")) {
+         seperator = '/';
+      } else if (input.contains("-")) {
+         seperator = '-';
+      } else {
+         seperator = ' ';
+      }
+      for(int i = 0; i < input.length(); i++) {
+         if(input.charAt(i) == seperator) {
+         hand.add(new Card(input.substring(lastSep, i)));
+         numCards++;
+         lastSep = i+1;
+         if(numCards == 4) {
+            hand.add(new Card(input.substring(lastSep)));
+            numCards++;
             }
-        }
-        return hand;
-    }
+         }
+      }
+      return hand;
+   }
+	
+	/**
+	 * Takes an ArrayList of Card objects and sorts them from lowest
+	 * to highest using an insertion sort
+	 * @param hand ArrayList of Card objects to be sorted
+	 * @return ArrayList of sort Cards
+	 */
+   public static ArrayList<Card> sort (ArrayList<Card> hand) {
+      int pos;
+      int i;
+      int key;
+      Card cKey;
 
-    public static ArrayList<Card> sort (ArrayList<Card> hand) {
-        int pos;
-        int i;
-        int key;
-        Card cKey;
-
-        for(pos = 1; pos < hand.size(); pos++) {
-            cKey = hand.get(pos);
-            key = cKey.getValueNum();
-            for(i = (pos-1); i >= 0; i--) {
-                Card currCard = hand.get(i);
-                if(currCard.getValueNum() > key || (currCard.getValueNum() == key && currCard.getSuitVal() > cKey.getSuitVal())) {
-                    hand.set(i+1, currCard);
-                    hand.set(i, cKey);
-                } else {
-                    i = -1;
-                }
+      for(pos = 1; pos < hand.size(); pos++) {
+         cKey = hand.get(pos);
+         key = cKey.getValueNum();
+         for(i = (pos-1); i >= 0; i--) {
+            Card currCard = hand.get(i);
+            if(currCard.getValueNum() > key || (currCard.getValueNum() == key && currCard.getSuitVal() > cKey.getSuitVal())) {
+               hand.set(i+1, currCard);
+               hand.set(i, cKey);
+            } else {
+               i = -1;
             }
-        }
-        return hand;
-    }
+         }
+      }
+      return hand;
+   }
     
-    public static boolean validate (ArrayList<Card> hand) {
-        for(int i = 0; i < hand.size(); i ++) {
-            for(int j = i+1; j < hand.size(); j++) {
-                if(hand.get(i).equals(hand.get(j))) {
-                    return false;
-                }
+	/**
+	 * Determines whether ArrayList of Cards contains any
+	 * cards of duplicate values.
+	 * @param hand ArrayList of cards
+	 * @return boolean representing whether hand is valid or not.
+	 */
+   public static boolean validate (ArrayList<Card> hand) {
+      for(int i = 0; i < hand.size(); i ++) {
+         for(int j = i+1; j < hand.size(); j++) {
+            if(hand.get(i).equals(hand.get(j))) {
+               return false;
             }
-        }
-        return true;
-    }
+         }
+      }
+      return true;
+   }
         
-    public static void combine (ArrayList<Card> hand) {
-        StringBuilder output = new StringBuilder();
-        for(int i = 0; i < hand.size(); i++) {
-            output.append(hand.get(i).toString());
-            if(i < hand.size() - 1) {
-                output.append(sep);
-            }
-        }
-        System.out.println(output.toString());
-    }
+	/**
+	 * Combines sorted ArrayList of cards back into a single string
+	 * representing a hand.
+	 * @param hand String representing sort hand
+	 */
+   public static void combine (ArrayList<Card> hand) {
+      StringBuilder output = new StringBuilder();
+      for(int i = 0; i < hand.size(); i++) {
+         output.append(hand.get(i).toString());
+         if(i < hand.size() - 1) {
+            output.append(" ");
+         }
+      }
+      System.out.println(output.toString());
+   }
 		
 }
