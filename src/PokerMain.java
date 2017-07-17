65d147351ce44c56eb3c441f96dddb58fd947248
Daniel Thomson <thoda061@student.otago.ac.nz>
2017-07-18 11:25:48 +1200

    final

--- src/PokerMain.java
+++ src/PokerMain.java
@@ -9,51 +9,63 @@ import java.util.regex.Pattern;
  */
 public class PokerMain {
 	
-	//Regular Expression for detecting correct format of input.
-   private static final String PATTERN ="(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC])";
-	
-	/**
-	 * Reads in line, determines whether it match regex, if it does separates cards
-	 * into ArrayList, sort them from lowest to highest, checks for duplicate
-	 * cards, then outputs sorted hand. If input does match regex or has 
-	 * duplicates prints invalid.
-	 * @param args arguments (not used)
-	 */
+   //Regular Expression for detecting correct format of input.
+   private static final String PATTERN ="(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSD"+
+      "HC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1"+
+      "-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJ"+
+      "Akqja])[sdhcSDHC][\\/\\s\\-])(([1-9]|([1][0-3])|[KQJAkqja])[sdhcSDHC])";
+   //Count for the number of seperator in the input
+   private static int sepCount;
+    
+   /**
+    * Reads in line, determines whether it match regex,
+    * if it does separates cards
+    * into ArrayList, sort them from lowest to highest, checks for duplicate
+    * cards, then outputs sorted hand. If input does match regex or has 
+    * duplicates prints invalid.
+    * @param args arguments (not used)
+    */
    public static void main(String[]args) {
       Scanner sc = new Scanner(System.in);
       while(sc.hasNextLine()) {
+         sepCount = 0;
          String input = sc.nextLine();
          boolean match = Pattern.matches(PATTERN, input);
          if(match) {
             ArrayList<Card> sepInput = seperate(input);
             sepInput = sort(sepInput);
-            if(!(validate(sepInput))) {
+            if(!(validate(sepInput))|| sepCount > 1 ) {
                System.out.println("Invalid: " + input);
             }else{
-					combine(sepInput);
-				}
+	       combine(sepInput);
+	    }
          } else {
             System.out.println("Invalid: " + input);
          }
       }
    }
 	
-	/**
-	 * Takes string as input and separates it in to individual Card objects
-	 * stored in ArrayList.
-	 * @param input String representing hand of cards.
-	 * @return ArrayList of Card objects.
-	 */
+   /**
+    * Takes string as input and separates it in to individual Card objects
+    * stored in ArrayList.
+    * @param input String representing hand of cards.
+    * @return ArrayList of Card objects.
+    */
    public static ArrayList<Card> seperate(String input) {
-      char seperator;
+      char seperator = '?';
       int lastSep = 0,numCards = 0;
       ArrayList<Card> hand = new ArrayList();
       if(input.contains("/")) {
          seperator = '/';
-      } else if (input.contains("-")) {
+         sepCount++;
+      }
+      if (input.contains("-")) {
          seperator = '-';
-      } else {
+         sepCount++;
+      }
+      if (input.contains(" ")) {
          seperator = ' ';
+         sepCount++;
       }
       for(int i = 0; i < input.length(); i++) {
          if(input.charAt(i) == seperator) {
@@ -69,12 +81,12 @@ public class PokerMain {
       return hand;
    }
 	
-	/**
-	 * Takes an ArrayList of Card objects and sorts them from lowest
-	 * to highest using an insertion sort
-	 * @param hand ArrayList of Card objects to be sorted
-	 * @return ArrayList of sort Cards
-	 */
+   /**
+    * Takes an ArrayList of Card objects and sorts them from lowest
+    * to highest using an insertion sort
+    * @param hand ArrayList of Card objects to be sorted
+    * @return ArrayList of sort Cards
+    */
    public static ArrayList<Card> sort (ArrayList<Card> hand) {
       int pos;
       int i;
@@ -86,7 +98,8 @@ public class PokerMain {
          key = cKey.getValueNum();
          for(i = (pos-1); i >= 0; i--) {
             Card currCard = hand.get(i);
-            if(currCard.getValueNum() > key || (currCard.getValueNum() == key && currCard.getSuitVal() > cKey.getSuitVal())) {
+            if(currCard.getValueNum() > key || (currCard.getValueNum() == key &&
+            currCard.getSuitVal() > cKey.getSuitVal())) {
                hand.set(i+1, currCard);
                hand.set(i, cKey);
             } else {
@@ -97,12 +110,12 @@ public class PokerMain {
       return hand;
    }
     
-	/**
-	 * Determines whether ArrayList of Cards contains any
-	 * cards of duplicate values.
-	 * @param hand ArrayList of cards
-	 * @return boolean representing whether hand is valid or not.
-	 */
+   /**
+    * Determines whether ArrayList of Cards contains any
+    * cards of duplicate values.
+    * @param hand ArrayList of cards
+    * @return boolean representing whether hand is valid or not.
+    */
    public static boolean validate (ArrayList<Card> hand) {
       for(int i = 0; i < hand.size(); i ++) {
          for(int j = i+1; j < hand.size(); j++) {
@@ -114,11 +127,11 @@ public class PokerMain {
       return true;
    }
         
-	/**
-	 * Combines sorted ArrayList of cards back into a single string
-	 * representing a hand.
-	 * @param hand String representing sort hand
-	 */
+   /**
+    * Combines sorted ArrayList of cards back into a single string
+    * representing a hand.
+    * @param hand String representing sort hand
+    */
    public static void combine (ArrayList<Card> hand) {
       StringBuilder output = new StringBuilder();
       for(int i = 0; i < hand.size(); i++) {
